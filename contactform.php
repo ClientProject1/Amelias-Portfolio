7dc119b28187b40d4a510abcdc23a0ad8a25f1b3
Jamie Phelps <james.phelps1995@live.com>
2020-04-29 14:57:23 +0100

    added amelia's email to mailto

--- contactform.php
+++ contactform.php
@@ -1,12 +1,15 @@
 <?php
 
+  //Does not work because it is localhost????
+  //does not work because it is gmail????
+
   if(isset($_POST['submit'])) {
     $fname = $_POST['first-name'];
     $sname = $_POST['second-name'];
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = "james.phelps1995@live.com";
+    $mailTo = "amelia.butler98@gmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have recieved an email from ".$fname." ".$sname.".\n\n".$message;
 
